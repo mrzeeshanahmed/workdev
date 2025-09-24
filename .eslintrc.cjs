86cd875b187fd8a0c8e411358a018b878312bf0b
Zeeshan Ahmed <ayaanahmed11@gmail.com>
2025-09-24 15:25:39 +0530

    Add backend/functions to tsconfig & ESLint; add CI type-check job

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,21 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: ['eslint:recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['backend/**/*.ts', 'functions/**/*.ts'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off'
+      }
+    }
+  ],
   rules: {
     'no-unused-vars': 'warn'
   }
